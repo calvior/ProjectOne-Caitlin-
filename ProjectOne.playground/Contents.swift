da712babbb5316bdeab9a54078c8209cc61bf3a7
calvior <calvior@hawaii.edu>
2018-01-25 13:48:16 -1000

    finished Variable Coding Challenge #1 and started #2. On part 5 of Project One

---
+++ ProjectOne.playground/Contents.swift
@@ -0,0 +1,23 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+//Variable Coding Challenge #1
+
+var firstString: String
+firstString = "I like to eat "
+
+let secondString = "watermelons"
+
+var thirdString = firstString + secondString
+print(thirdString)
+
+
+//Variable Coding Challenge #2
+var celsius: Float
+
+let fahrenheit: Float ; "50"
+celsius = 5/9*(fahrenheit-32)
+print("Today it is \(celsius) degrees celsius")
