89dfec329fc104487ca648061e60d84268d52e30
calvior <calvior@hawaii.edu>
2018-02-07 15:11:16 -1000

    project 1 completed

--- ProjectOne.playground/Contents.swift
+++ ProjectOne.playground/Contents.swift
@@ -16,8 +16,38 @@ print(thirdString)
 
 
 //Variable Coding Challenge #2
+
 var celsius: Float
 
-let fahrenheit: Float ; "50"
-celsius = 5/9*(fahrenheit-32)
+let fahrenheit = Float("50")
+celsius == (fahrenheit - 32) * 5/9
+
 print("Today it is \(celsius) degrees celsius")
+//I couldn't quite figure out how to make it work :(
+
+
+//Boolean Variables Coding Challenge #1
+
+let numberOne = 27
+let numberTwo = 45
+let numberThree = numberOne == numberTwo
+
+
+//If Statement Coding Challenge #1
+
+let b = "Sloths are cool"
+let bLength = b.characters.count
+print(bLength)
+
+let c = "Flat Stanely"
+let cLength = c.characters.count
+print(cLength)
+
+if b > c {
+    print("nice job")
+}
+else {
+    print("nope")
+}
+
+//I still have one error and two warnings but I can't find where they are anymore. I think the error is under the Variable Coding #2, something is wrong my equation. I have no idea where the warnings are from (probably the same place).
